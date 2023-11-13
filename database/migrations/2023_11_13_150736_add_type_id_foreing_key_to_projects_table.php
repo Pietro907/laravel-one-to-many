c0ce143b9c570b3a96ff3f5a1f809da020fbb45b
Pietro907 <pietrop.puri@gmail.com>
2023-11-13 16:13:42 +0100

    add model Type & migrazion types_to_projects_table

---
+++ database/migrations/2023_11_13_150736_add_type_id_foreing_key_to_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            
+            $table->unsignedBigInterger('type_id')->nullabe()->after('id');
+            $table->forening('type_id')
+            ->references8('id')
+            ->on('types');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+    }
+};
